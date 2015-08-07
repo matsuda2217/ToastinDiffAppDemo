ac002a8fc2272e98394ceb6ba1b2336ba05ffed1
matsuda2217 <thuctran2217@gmail.com>
2015-08-07 15:38:37 +0700

    UP

--- app/src/main/java/com/example/tt/toastindiffappdemo/MainActivity.java
+++ app/src/main/java/com/example/tt/toastindiffappdemo/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.tt.toastindiffappdemo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -13,25 +17,72 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void showToastTopLeft(View view) {
+        EditText e1 = (EditText) findViewById(R.id.editText1);
+        EditText e2 = (EditText) findViewById(R.id.editText2);
+        if (e1.getText().toString().trim().length() > 0 &&
+                e2.getText().toString().trim().length() > 0) {
+            String editT1 = e1.getText().toString();
+            String editT2 = e2.getText().toString();
+            int xOffSet = Integer.parseInt(editT1);
+            int yOffSet = Integer.parseInt(editT2);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            Toast toast = Toast.makeText(getApplicationContext(), "Hello Toast", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.TOP| Gravity.LEFT, xOffSet, yOffSet);
+            toast.show();
+        } else {
+            Toast.makeText(getApplicationContext(),"Please enter xOffSet & yOffSet",Toast.LENGTH_SHORT).show();
         }
+    }
+    public void showToastTopRight(View view) {
+        EditText e1 = (EditText) findViewById(R.id.editText1);
+        EditText e2 = (EditText) findViewById(R.id.editText2);
+        if (e1.getText().toString().trim().length() > 0 &&
+                e2.getText().toString().trim().length() > 0) {
+            String editT1 = e1.getText().toString();
+            String editT2 = e2.getText().toString();
+            int xOffSet = Integer.parseInt(editT1);
+            int yOffSet = Integer.parseInt(editT2);
 
-        return super.onOptionsItemSelected(item);
+            Toast toast = Toast.makeText(getApplicationContext(), "Hello Toast", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.TOP | Gravity.RIGHT, xOffSet, yOffSet);
+            toast.show();
+        } else {
+            Toast.makeText(getApplicationContext(),"Please enter xOffSet & yOffSet",Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void showToastBottomLeft(View view) {
+        EditText e1 = (EditText) findViewById(R.id.editText1);
+        EditText e2 = (EditText) findViewById(R.id.editText2);
+        if (e1.getText().toString().trim().length() > 0 &&
+                e2.getText().toString().trim().length() > 0) {
+            String editT1 = e1.getText().toString();
+            String editT2 = e2.getText().toString();
+            int xOffSet = Integer.parseInt(editT1);
+            int yOffSet = Integer.parseInt(editT2);
+
+            Toast toast = Toast.makeText(getApplicationContext(), "Hello Toast", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM | Gravity.LEFT, xOffSet, yOffSet);
+            toast.show();
+        } else {
+            Toast.makeText(getApplicationContext(),"Please enter xOffSet & yOffSet",Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void showToastBottomRight(View view) {
+        EditText e1 = (EditText) findViewById(R.id.editText1);
+        EditText e2 = (EditText) findViewById(R.id.editText2);
+        if (e1.getText().toString().trim().length() > 0 &&
+                e2.getText().toString().trim().length() > 0) {
+            String editT1 = e1.getText().toString();
+            String editT2 = e2.getText().toString();
+            int xOffSet = Integer.parseInt(editT1);
+            int yOffSet = Integer.parseInt(editT2);
+
+            Toast toast = Toast.makeText(getApplicationContext(), "Hello Toast", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM | Gravity.RIGHT, xOffSet, yOffSet);
+            toast.show();
+        } else {
+            Toast.makeText(getApplicationContext(),"Please enter xOffSet & yOffSet",Toast.LENGTH_SHORT).show();
+        }
     }
 }
